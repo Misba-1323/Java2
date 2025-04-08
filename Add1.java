b68068b7f742ea53eff19a1ebeda27669cc09009
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-08 12:09:54 +0530

    save Add1.java in master

--- Add1.java
+++ Add1.java
@@ -6,7 +6,11 @@ public class Add1 {
         int b = 15;
         int add = a + b;
         int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
         System.out.println("Addition = "+add);
         System.out.println("Subtraction = "+sub);
+        System.out.println("Multiplication = "+mul);
+        System.out.println("Division = "+div);
     }
 }
