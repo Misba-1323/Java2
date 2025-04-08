fe49096cdb4cf01b092c416fb4e018a2d9802bad
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-08 12:06:27 +0530

    save Add1.java in main

---
+++ Add1.java
@@ -0,0 +1,12 @@
+package Java2;
+
+public class Add1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+        int add = a + b;
+        int sub = a - b;
+        System.out.println("Addition = "+add);
+        System.out.println("Subtraction = "+sub);
+    }
+}
